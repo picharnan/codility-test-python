1afa3e86c068bc6cf83cc0abd9bb007ece99f116
Picharnan Buaporn <picharnan@live.com>
2022-02-27 09:57:00 +0700

    100% CountDiv

--- lesson05_prefix_sums/CountDiv.py
+++ lesson05_prefix_sums/CountDiv.py
@@ -1,7 +1,9 @@
+import math
+
 def solution(A, B, K):
     res = 0
 
-    res = (B / K) - (A / K) 
+    res = math.floor(B / K) - math.floor(A / K) 
 
     if A % K == 0 :
         res += 1
@@ -9,8 +11,8 @@ def solution(A, B, K):
     return int(res)
 
 # print(solution(2, 2, 2))
-# print(solution(0, 14, 2))
-print(solution(0, 14, 3))
+print(solution(0, 14, 2))
+# print(solution(0, 14, 3))
 # print(solution(6, 11, 2))
 # print(solution(6, 11, 7))
 # print(solution(6, 11, 3))
